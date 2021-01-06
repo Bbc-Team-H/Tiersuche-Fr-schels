3e900e901f57b8d9e07847782292b2e4e3892aa8
Noël Arn <76202463+zuwyld@users.noreply.github.com>
2021-01-06 09:37:56 +0100

    kp

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const menuButton = document.getElementById("menu")
 const nav = document.querySelector("nav")
+const back = document.querySelector("#back")
+
 const toggleMenuAndOverlay = () => {
     nav.classList.toggle("visible")
 }
+
+const menuback = () => {
+    nav.classList.toggle("visible")
+}
+
+back.addEventListener("click", menuback)
+
 menuButton.addEventListener("click", toggleMenuAndOverlay)
